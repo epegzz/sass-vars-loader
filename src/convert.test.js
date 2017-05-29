1d9f96575a67728a833a45769f1b962849cfc36a
Jack Buckland <the.ninja.dev@gmail.com>
2017-05-29 18:30:24 +0100

    Update tests to ensure nested lists work as intended.

--- src/convert.test.js
+++ src/convert.test.js
@@ -34,14 +34,36 @@ test('Convert number', function() {
   expect(convert(obj, opts)).toBe(out);
 });
 
-test('Convert array', function() {
+test('Convert array: simple', function() {
   const obj = {test: [1, 2]};
   const opts = {
     prefix: "$",
     suffix: ";",
     suffixLastItem: true,
   };
-  const out = '$test:1, 2;';
+  const out = '$test:(1, 2);';
+  expect(convert(obj, opts)).toBe(out);
+});
+
+test('Convert array: nested arrays', function() {
+  const obj = {test: [["list", 1], ["list", 2]]};
+  const opts = {
+    prefix: "$",
+    suffix: ";",
+    suffixLastItem: true,
+  };
+  const out = '$test:((list, 1), (list, 2));';
+  expect(convert(obj, opts)).toBe(out);
+});
+
+test('Convert array: nested object with array', function() {
+  const obj = {test: [15, "px", {"nested": ["oh", "no"]}]};
+  const opts = {
+    prefix: "$",
+    suffix: ";",
+    suffixLastItem: true,
+  };
+  const out = '$test:(15, px, (\nnested:(oh, no)\n));';
   expect(convert(obj, opts)).toBe(out);
 });
 
@@ -75,6 +97,17 @@ test('Convert object: nested', function() {
     suffixLastItem: true,
   };
   const out = '$test:(\na:1,\nb:2,\nc:(\nd:4\n)\n);';
+  expect(convert(obj, opts)).toBe(out);
+});
+
+test('Convert object: nested with array', function() {
+  const obj = {test: {a: 1, b: 2, c: {d: [4, "px", "em"]}}};
+  const opts = {
+    prefix: "$",
+    suffix: ";",
+    suffixLastItem: true,
+  };
+  const out = '$test:(\na:1,\nb:2,\nc:(\nd:(4, px, em)\n)\n);';
   expect(convert(obj, opts)).toBe(out);
 });
 
