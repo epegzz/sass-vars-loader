30f7d8a99c1520f7eae75d6bf153cabcf3d8ea76
Daniel <epegzz@gmail.com>
2017-11-18 00:37:21 +0100

    Reformat code

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,35 +1,44 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  watch: true,
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        // Inserts all imported styles into the html document
-        { loader: "style-loader" },
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // Inserts all imported styles into the html document
+          { loader: 'style-loader' },
 
-        // Translates CSS into CommonJS
-        { loader: "css-loader" },
+          // Translates CSS into CommonJS
+          { loader: 'css-loader' },
 
-        // Compiles Sass to CSS
-        { loader: "sass-loader", options: { includePaths: ["app/styles.scss"] } },
-
-        // Reads Sass vars from files or inlined in the options property
-        { loader: "@epegzz/sass-vars-loader", options: {
-          // Option 1) Specify vars here
-          vars: {
-            greenFromWebpackConfig: 'green'
+          // Compiles Sass to CSS
+          {
+            loader: 'sass-loader',
+            options: { includePaths: ['app/styles.scss'] }
           },
-          files: [
-            // Option 2) Load vars from JSON file
-            path.resolve(__dirname, 'config/sassVars.json'),
-            // Option 3) Load vars from JS file
-            path.resolve(__dirname, 'config/sassVars.js')
-          ]
-        }
-      }]
-    }]
+
+          // Reads Sass vars from files or inlined in the options property
+          {
+            loader: '@epegzz/sass-vars-loader',
+            options: {
+              // Option 1) Specify vars here
+              vars: {
+                greenFromWebpackConfig: 'green'
+              },
+              files: [
+                // Option 2) Load vars from JSON file
+                path.resolve(__dirname, 'config/sassVars.json'),
+                // Option 3) Load vars from JS file
+                path.resolve(__dirname, 'config/sassVars.js')
+              ]
+            }
+          }
+        ]
+      }
+    ]
   },
   output: {
     filename: 'bundle.js',
